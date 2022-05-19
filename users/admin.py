e2372bc94c6f6750ad29d2352767c6b5e4395981
Hakimkhon Sharifkhonov <root@chasys.local>
2022-05-19 17:18:44 +0500

    testing

--- users/admin.py
+++ users/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-    # fields = ['first_name', 'last_name', 'is_staff', 'password']
     list_display = ['id','first_name', 'last_name', 'is_staff', 'email']
     list_display_links = list_display # ['id','first_name', 'last_name']
+    readonly_fields = ['password']
+    fieldsets = (
+    # fields = ['first_name', 'last_name', 'is_staff', 'password']
     # list_filter = ['first_name']
     # list_editable = ['first_name', 'last_name', 'email']
     # search_fields = ['first_name']
-    readonly_fields = ['password']
-    fieldsets = (
         ('Personal Information', {
             "fields": (
                 ('first_name', 'last_name'),
